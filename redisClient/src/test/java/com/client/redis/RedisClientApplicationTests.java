72caec8990ea08f7c02fb66c2f133180650fee3b
umesh sanwal <umeshsnwl@gmail.com>
2024-04-17 21:44:46 +0530

    starter

---
+++ redisClient/src/test/java/com/client/redis/RedisClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.client.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
